48600c9d1885c0e8854c4f0aa1e2fe51f5de597b
Nikita <nikita.kopeickin@yandex.ru>
2023-01-24 15:26:15 +0300

    second number

---
+++ summN/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число");
+var number = Convert.ToInt64(Console.ReadLine());
+
+int i = 1;
+int summ = 0;
+System.Console.WriteLine("Вывод cуммы всех чисел от 1 до " + number);
+
+while (i <= number)
+{
+    summ += i; 
+    i++;
+}
+System.Console.WriteLine(summ);
