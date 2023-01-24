c222d9bbd5c150eedaddd4b88a115da88eb5c071
Nikita <nikita.kopeickin@yandex.ru>
2023-01-24 15:42:55 +0300

    fourth

---
+++ summNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summ = 0;
+
+while (number > 0)
+{
+    summ = summ + number % 10;
+    number = number / 10;
+}
+
+System.Console.WriteLine(summ);
